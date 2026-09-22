2d04faba3047ba169a2ab82376a0e6d90cc42f7e
TPh <placeholder-email@gmail.com>
2026-09-22 15:11:48 -0400

    Professor Reviewed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 import {Comic} from './shared/models/comic';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
