3c66259750a5eadf08234a223ee9b7f621f1c012
TPh <placeholder-email@gmail.com>
2026-09-22 01:00:01 -0400

    Item List, for, if, else, empty, Display HTML.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Comic} from './shared/models/comic';
 
 @Component({
   imports: [RouterOutlet],
@@ -11,8 +12,68 @@ export class App {
 
   title = signal('Angular-Project');
 
-  /* Code for the components here */
-  firstMessage:string = "HELLO WORLD";
-  isFirstAngularProject: boolean = true;
+  /* Comic Array List */
+  comicList: Comic[] = [
+    {
+      id: 1,
+      title: 'One Piece',
+      author: ['Eiichiro Oda'],
+      genre: ['Adventure', 'Action'],
+      digitalAvailable: true,
+      printAvailable: 1000000,
+      isOnHiatus: false,
+    },
+    {
+      id: 2,
+      title: 'Naruto ',
+      author: ['Masashi Kishimoto'],
+      genre: ['Ninja', 'Shounen'],
+      digitalAvailable: true,
+      printAvailable: true,
+      isOnHiatus: false,
+    },
+    {
+      id: 3,
+      title: 'Hunter x Hunter',
+      author: ['Yoshihiro Togashi'],
+      genre: ['Action', 'Shounen'],
+      digitalAvailable: true,
+      printAvailable: true,
+      isOnHiatus: true,
+    },
+    {
+      id: 4,
+      title: 'Dragon Ball',
+      author: ['Akira Toriyama'],
+      genre: ['Adventure', 'Shounen'],
+      digitalAvailable: true,
+      printAvailable: true,
+      isOnHiatus: false,
+    },
+    {
+      id: 5,
+      title: 'Death Note',
+      author: ['Tsugumi Ohba', 'Takeshi Obata'],
+      genre: ['Thriller', 'Detective'],
+      digitalAvailable: false,
+      printAvailable: false,
+      isOnHiatus: false,
+    },
+    {
+      id: 6,
+      title: 'One-Punch Man',
+      author: ['One', 'Yusuke Murata'],
+      genre: ['Action', 'Comedy'],
+      digitalAvailable: true,
+      printAvailable: true,
+      isOnHiatus: false,
+    },
+  ];
 
+  // Let the parent component react to a card being opened
+  onComicOpened(comic: Comic): void {
+    /* Place holder for now */
+    console.warn("Opened: ", comic.title)
+
+  }
 }
--- src/app/shared/models/comic.ts
+++ src/app/shared/models/comic.ts
@@ -2,10 +2,8 @@ export interface Comic {
   id: number;
   title: string;
   author: string[];
-  artist: string[];
   genre: string[];
-  theme: string[];
-  digitalAvailable: boolean | number; /* number to show specific stock count */
-  paperbackAvailable?: boolean | number; /* number to show specific stock count */
+  digitalAvailable: boolean;
+  printAvailable: boolean | number;
   isOnHiatus?: boolean;
 }
